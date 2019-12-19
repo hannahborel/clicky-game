f19cc1c74268edbade0d5a4970484ebf2cc8bffe
Hannah Borel <hannahborel>
2019-12-18 20:21:23 -0600

    Updates

---
+++ precache-manifest.8f59b3819d038ba0be7cee546d159a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ca08460b26877f3ff116f2f23775d8",
+    "url": "%7Bhannahborel%7D.github.io/%7Bclicky-game%7D/index.html"
+  },
+  {
+    "revision": "45044768ace3c9c765a7",
+    "url": "%7Bhannahborel%7D.github.io/%7Bclicky-game%7D/static/css/main.14f2c155.chunk.css"
+  },
+  {
+    "revision": "d3ef8ca54e90418bc929",
+    "url": "%7Bhannahborel%7D.github.io/%7Bclicky-game%7D/static/js/2.e182bf31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "%7Bhannahborel%7D.github.io/%7Bclicky-game%7D/static/js/2.e182bf31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45044768ace3c9c765a7",
+    "url": "%7Bhannahborel%7D.github.io/%7Bclicky-game%7D/static/js/main.fa05ce64.chunk.js"
+  },
+  {
+    "revision": "e9ac20d2df953c609d5a",
+    "url": "%7Bhannahborel%7D.github.io/%7Bclicky-game%7D/static/js/runtime-main.a40fafd8.js"
+  }
+]);
